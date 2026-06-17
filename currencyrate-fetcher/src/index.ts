04e7904904ebb469f94b4f462ce8ced687ebe22b
emarkula24 <emarkula24@student.oulu.fi>
2026-06-17 22:43:25 +0300

    Update cors domain for new github page

--- currencyrate-fetcher/src/index.ts
+++ currencyrate-fetcher/src/index.ts
@@ -21,7 +21,7 @@ export interface Env {
 }
 
 const corsHeaders = {
-	"Access-Control-Allow-Origin": "https://emarkula24.github.io", // or specify a domain instead of "*"
+	"Access-Control-Allow-Origin": "https://nobootloader.github.ioo",
 	"Access-Control-Allow-Methods": "GET, OPTIONS",
 	"Access-Control-Allow-Headers": "Content-Type",
 }
